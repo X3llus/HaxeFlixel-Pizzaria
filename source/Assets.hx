dc57f0846513871fd17f0cf235d7a9b9bc8d6492
csc530 <{ID}+{username}@users.noreply.github.com>
2023-02-10 14:05:25 -0500

    🐛 removed undefined volume var

--- source/Assets.hx
+++ source/Assets.hx
@@ -3,17 +3,18 @@ import flixel.FlxG;
 import flixel.system.FlxSound;
 
  class AssetsUtil {
-	public static final successSfx = FlxG.sound.load(AssetPaths.yay_6120__mp3, volume);
+
+	public static final successSfx = FlxG.sound.load(AssetPaths.yay_6120__mp3);
 	public static final playSuccess = (volume:Float = 1) ->{
 		successSfx.volume = volume;
 		successSfx.play(true);
 	};
-	public static final failSfx = FlxG.sound.load(AssetPaths.negative_beeps_6008__mp3, volume);
+	public static final failSfx = FlxG.sound.load(AssetPaths.negative_beeps_6008__mp3);
 	public static final playFail = (volume:Float = 1) ->{
 		failSfx.volume = volume;
 		failSfx.play(true);
 	};
-	public static final bgMusic = FlxG.sound.load(AssetPaths.upbeat_funky_retro_131297__mp3, volume);
+	public static final bgMusic = FlxG.sound.load(AssetPaths.upbeat_funky_retro_131297__mp3);
 	public static final playBGMusic = (volume:Float = 1) -> {
 		bgMusic.volume = volume;
 		bgMusic.play(true);
