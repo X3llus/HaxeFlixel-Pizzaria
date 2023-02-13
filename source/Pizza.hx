afc05e2361fcaeb203c8ac9262ca143f2a3cb17c
csc530 <{ID}+{username}@users.noreply.github.com>
2023-02-13 14:16:05 -0500

    🍱 Add pizzas without sauce
    More structuring of directories
    and added new conditional branch to render pizza with no sauce

--- source/Pizza.hx
+++ source/Pizza.hx
@@ -54,53 +54,78 @@ class Pizza extends FlxExtendedSprite
 		var hasPepperoni = toppings.contains(ToppingEnum.pepperoni);
 		var hasSauce = toppings.contains(ToppingEnum.light_sauce) || toppings.contains(ToppingEnum.dark_sauce);
 		var hasOnlyCheese = hasCheese && !hasMushroom && !hasPepperoni;
-		var hasOneTopping = (hasMushroom && !hasPepperoni) || (hasPepperoni && !hasMushroom); //XOr
+		var hasOneTopping = (hasMushroom && !hasPepperoni) || (hasPepperoni && !hasMushroom); // XOr
 
 		// ? The file name of the asset to display
-		var asset = "assets/images/pizzas/";
+		var assetPath = "assets/images/pizzas/";
 		var pizza = "";
 		if (cooked)
-			{
-				pizza+="cooked";
-				asset += "cooked/";}
-		else
-			{
-				pizza+="pizza";asset += "raw/";}
-
-		if(hasOnlyCheese && hasSauce)
-			asset+="cheese-only/"
-		else if(hasSauce &&hasOneTopping)
-			{
-				asset+="one-topping/";
-				if(!hasCheese)
-					asset+="no-cheese/";
-				else if(hasMushroom)
-					asset+="mushroom/";
-				else if(hasPepperoni)
-					asset+="pepperoni/";
-			}
-			else if(hasPepperoni && hasMushroom)
-			{
-				asset+="two-toppings/";
-			// so far only 1 variant for sauce colours for cooked pizza
-				if(!cooked){
-			if (!hasCheese)
-				asset += "no-cheese/";
-			else if (toppings.contains(ToppingEnum.light_sauce))
-				asset+="light-sauce/";
-			else if (toppings.contains(ToppingEnum.dark_sauce))
-				asset+="dark-sauce/";
+		{
+			pizza += "cooked";
+			assetPath += "cooked/";
 		}
+		else
+		{
+			pizza += "pizza";
+			assetPath += "raw/";
+		}
+
+		// find correct path
+		if (!hasSauce)
+		{
+			assetPath += "no-sauce/";
+			if (!cooked)
+			{
+				if (hasMushroom && hasPepperoni)
+					assetPath += "two-toppings/";
+				else if (hasPepperoni)
+					assetPath += "pepperoni/";
+				else if (hasMushroom)
+					assetPath += "mushroom/";
+				else if (hasCheese)
+					assetPath += "cheese-only/";
 			}
+			else
+			{
+				if (hasMushroom && hasPepperoni)
+					assetPath += "two-toppings/";
+				else if (hasOneTopping)
+					assetPath += "one-topping/";
+			}
+		}
+		else if (hasOnlyCheese && hasSauce)
+			assetPath += "cheese-only/"
+		else if (hasSauce && hasOneTopping)
+		{
+			assetPath += "one-topping/";
+			if (!hasCheese)
+				assetPath += "no-cheese/";
+			else if (hasMushroom)
+				assetPath += "mushroom/";
+			else if (hasPepperoni)
+				assetPath += "pepperoni/";
+		}
+		else if (hasPepperoni && hasMushroom)
+		{
+			assetPath += "two-toppings/";
+			// so far only 1 variant for sauce colours for cooked pizza
+			if (!cooked)
+			{
+				if (!hasCheese)
+					assetPath += "no-cheese/";
+				else if (toppings.contains(ToppingEnum.light_sauce))
+					assetPath += "light-sauce/";
+				else if (toppings.contains(ToppingEnum.dark_sauce))
+					assetPath += "dark-sauce/";
+			}
+		}
 
-
+		// find correct pizza image
 		// so far only 1 variant for sauce colours for cooked pizza
-			if(!cooked){
 		if (toppings.contains(ToppingEnum.light_sauce))
 			pizza += "-ls";
 		else if (toppings.contains(ToppingEnum.dark_sauce))
 			pizza += "-ds";
-	}
 
 		if (toppings.contains(ToppingEnum.yellow_cheese))
 			pizza += "-yc";
@@ -112,7 +137,6 @@ class Pizza extends FlxExtendedSprite
 		if (toppings.contains(ToppingEnum.mushroom))
 			pizza += "-m";
 
-
-		loadGraphic(asset+pizza+".png", false, 100, 100);
+		loadGraphic(assetPath + pizza + ".png", false, 100, 100);
 	}
 }
