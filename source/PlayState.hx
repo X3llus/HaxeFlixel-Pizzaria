81bb6f60167ad3fa418b2ce2662b3416e0f3d474
Mason8766 <77406208+Mason8766@users.noreply.github.com>
2023-02-13 11:23:09 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -24,36 +24,43 @@ class PlayState extends FlxState
 	{
 		super.create();
 
-		// Delete all below this, Just a demo of how it works
+		//Delete all below this, Just a demo of how it works
 		var pizzaOne = newOrder(5);
-		trace(pizzaOne.pId);
-		trace(pizzaOne.pSauce);
-		trace(pizzaOne.pBake);
-		trace(pizzaOne.pTopping);
+		// trace(pizzaOne.pId);
+		// trace(pizzaOne.pSauce);
+		// trace(pizzaOne.pBake);
+		// trace(pizzaOne.pTopping);
 
-		var pizzaTwo = newOrder(3);
+		var pizzaTwo = newOrder(2);
 		trace(pizzaTwo.pId);
 		trace(pizzaTwo.pSauce);
-		trace(pizzaTwo.pBake);
+		trace(pizzaTwo.pCheese);
 		trace(pizzaTwo.pTopping);
-
-		var t = new flixel.text.FlxText(0, 0, 0, "", 64);
-		var t2 = new flixel.text.FlxText(0, 0, 0, "", 64);
-		var t3 = new flixel.text.FlxText(0, 0, 0, "", 12);
+		trace(pizzaTwo.ordArray);
+	
+		var t = new flixel.text.FlxText(0,0,0,"",64);
+		var t2 = new flixel.text.FlxText(0,0,0,"",64);
+		var t3 = new flixel.text.FlxText(0,0,0,"",12);
+		var t4 = new flixel.text.FlxText(0,0,0,"",12);
 
 		t.screenCenter();
-		add(t);
-		t.visible = false;
+			add(t);
+			t.visible = false;
 
-		t2.screenCenter();
-		add(t2);
-		t2.visible = false;
+			t2.screenCenter();
+			add(t2);
+			t2.visible = false;
 
-		t3.screenCenter();
-		add(t3);
-		t3.visible = true;
+			t3.screenCenter();
+			add(t3);
+			t3.visible = false;
 
-		pizzaTwo.displayOrder(pizzaTwo, t, t2, t3);
+			t4.screenCenter();
+			add(t4);
+			t4.visible = true;
+
+
+		pizzaTwo.displayOrder(pizzaTwo,t,t2,t3,t4);
 	}
 
 	override public function update(elapsed:Float)
@@ -76,64 +83,67 @@ class PlayState extends FlxState
 		}
 	}
 
-	public function newOrder(complexity:Int):PizzaOrder
-	{
-		pId += 1;
-		// Init all flags
+	public function newOrder(complexity:Int):PizzaOrder{
+		pId +=1;
+		//Init all flags
 		var pSauce = "";
-		var pBake = "";
+		
+		var pCheese = "";
 		var pTopping:Array<String> = [];
+		var ordArray:Array<String> = [];
 
-		while (complexity > 0)
-		{ // create pizza loop
-			// if complexity is higher then offering, set to max offerings
-			if (complexity > 5)
-				complexity = 5;
+		while (complexity> 0 ){//create pizza loop
+			//if complexity is higher then offering, set to max offerings
+			if(complexity >4 )
+				complexity = 4;
 
-			var x = randomRangeInt(1, 8); // Decides on random topping
+			var x = randomRangeInt(1,6); //Decides on random topping
+			
+			if (x == 1 && pSauce == ""){//random sauce
+				complexity -=1;
+				pSauce = "light_sauce";
+				
 
-			if (x == 1 && pSauce == "")
-			{ // random sauce
-				complexity -= 1;
-				x = randomRangeInt(1, 2);
-				if (x == 1)
-					pSauce = "Light";
-				else
-					pSauce = "Extra";
 			}
-			if (x == 2 && pBake == "")
-			{ // Welldone Bake
-				complexity -= 1;
-				pBake = "Well Done";
+			if(x == 2 && pCheese =="" ){//Pick Special Cheese
+				complexity -=1;
+				pCheese = "yellow_Cheese";
 			}
-			if (x > 2)
-			{ // random Topping
-				x = randomRangeInt(1, 3);
-				if (x == 1 && pTopping.contains("Topping1") == false)
-				{
-					pTopping.push("Topping1");
-					complexity -= 1;
+			
+
+			if(x>3){//random Topping
+				x = randomRangeInt(1,2);
+				if(x == 1 && pTopping.contains("mushroom") == false){
+					pTopping.push("mushroom");
+					complexity -=1;
 				}
-				if (x == 2 && pTopping.contains("Topping2") == false)
-				{
-					pTopping.push("Topping2");
-					complexity -= 1;
+				if(x == 2 && pTopping.contains("pepperoni")== false){
+					pTopping.push("pepperoni");
+					complexity -=1;
 				}
-				if (x == 3 && pTopping.contains("Topping3") == false)
-				{
-					pTopping.push("Topping3");
-					complexity -= 1;
-				}
+				
+
 			}
 		}
-		// if no value set, set a defult
-		if (pSauce == "")
-			pSauce = "Regular";
-		if (pBake == "")
-			pBake = "Regular";
+		//if no value set, set a defult 
+		if(pSauce == "")
+			pSauce = "dark_sauce";
+		
+		if(pCheese == "")
+			pCheese = "White_Cheese";
 
-		// pizzaorder made and returned
-		var pizza = new PizzaOrder(pId, pSauce, pBake, pTopping);
+		//pizzaorder made and returned
+		var pizza = new PizzaOrder(pId,pSauce, pCheese, pTopping, ordArray);
+
+		ordArray.push(pSauce);
+		ordArray.push(pCheese);
+
+		for (top in pTopping){//each topping
+				ordArray.push(top);
+			}
+	
+
+
 		return pizza;
 	}
 }
