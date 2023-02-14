79d2bf1600e9f8518a4362fd3860e8bbe748257c
NikBrennan <ndbrenna@lakeheadu.ca>
2023-02-14 14:07:45 -0500

    Commented code removed.

--- source/Pizza.hx
+++ source/Pizza.hx
@@ -34,15 +34,6 @@ class Pizza extends FlxExtendedSprite
 		enableMouseClicks(false);
 		enableMouseDrag();
 		toppingSprites = toppingGroup;
-		// mouseStartDragCallback = (_, _, _) ->
-		// {
-		// 	toppingGroup.clear();
-		// 	if (cooked)
-		// 		loadGraphic(AssetPaths.pizzabox__png, false, 100, 100);
-		// 	else
-		// 		loadGraphic(AssetPaths.tray__png, false, 100, 100);
-		// }
-		// mouseStopDragCallback = (_, _, _) -> updateGraphic();
 	}
 
 	/**
@@ -76,26 +67,9 @@ class Pizza extends FlxExtendedSprite
 	 */
 	public function updateGraphic(topping:ToppingEnum, cooked:Bool = false)
 	{
-		// if (cooked)
-		// 	loadGraphic(AssetPaths.cooked_dough__png);
-		// else
-		// 	loadGraphic(AssetPaths.round_dough__png);
-
 		var tempTopping:ToppingSprite;
 		tempTopping = new ToppingSprite(topping, cooked);
 
-		// var padding = 10;
-		// // place cooked sauces in the center of the pizza
-		// if (cooked && (topping == ToppingEnum.dark_sauce || topping == ToppingEnum.light_sauce))
-		// {
-		// 	tempTopping.x = x + (width - tempTopping.width) / 2;
-		// 	tempTopping.y = y + (height - tempTopping.height) / 2;
-		// }
-		// else // place topping randomly on top and within of the pizza
-		// {
-		// 	tempTopping.x = FlxG.random.float(x + padding, x + width - tempTopping.width - padding);
-		// 	tempTopping.y = FlxG.random.float(y + padding, y + height - tempTopping.height - padding);
-		// }
 		// place ingredient sprite over the pizza
 		var xPos:Int = Std.int(((this.width - tempTopping.width) / 2));
 		var yPos:Int = Std.int(((this.height - tempTopping.height) / 2));
